874a874f2dac5c57e370148cbb37ae5cd3d5f5cd
turuthivic <turuthivictor9@gmail.com>
2020-11-19 07:20:09 +0000

    some changes to controller" git push

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -2,11 +2,13 @@
 require 'mambu_api' 
 class ClientsController < ApplicationController
   before_action :authorize_client, except: :landing_page
-  @@base_uri = 'https://finplus.sandbox.mambu.com/api/clients'
-  @@users_uri = 'https://finplus.sandbox.mambu.com/api/users'
+  @@base_uri = 'https://finplus.sandbox.mambu.com/api/'
+#   @@users_uri = 'https://finplus.sandbox.mambu.com/api/users'
   
   def landing_page
-    authorize_request
+    # authorize_request
+    # @user_key = helpers.get_details[:user_key]
+    @user_key = helpers.get_details
   end
 
   def index
@@ -18,7 +20,7 @@ class ClientsController < ApplicationController
   end
   
   def create
-    @client = find_clients(@@base_uri)
+    @client = find_clients(@@base_uri+"clients")
   end
   
   def show
@@ -29,7 +31,7 @@ private
 
   def find_clients(base_uri)
     api = MambuApi.new
-    api.request_client_basic_auth(@@base_uri)
+    api.request_api(@@base_uri+"clients")
   end
   
   def authorize_request
@@ -37,7 +39,10 @@ private
   end
   
   def valid_url?
-    !!(request.url).match(/https:\/\/finplus.sandbox.mambu.com/)
+    # extract the signed url
+    # !!(request.url).match(/https:\/\/finplus.sandbox.mambu.com/)
+    tenant = helpers.get_details[:tenant]
+    true if tenant == "finplus"
   end
   
   def names
@@ -45,15 +50,16 @@ private
   end
 
   def authorize_client
-    client = request_client_encoded_key(@@users_uri, user_key) 
-    username = client["userName"]
-    render landing_page unless names.include? username   
+    client = request_client_encoded_key
+    username = client["username"]
+    render landing_page unless names.include? username
   end
   
-  def request_client_encoded_key(uri, user_key)
-    response = HTTParty.get(
-      @@users_uri+"/#{user_key}",
-      )
+  def request_client_encoded_key
+     # get_details from client helper
+     user_key = helpers.get_details[:user_key].to_s
+     api = MambuApi.new
+     response = api.request_api(@@base_uri+"users/"+user_key)
   end
 end
 
--- app/helpers/client_helper.rb
+++ app/helpers/client_helper.rb
@@ -1,12 +1,15 @@
 require 'base64'
 module ClientHelper
-  def get_details
-    values = Base64.decode64(
+  def get_details()
+    output = Base64.decode64(
       'eyJET01BSU4iOiJmaW5wbHVzLnNhbmRib3gubWFtYnUuY29tIiwiQUxHT1JJVEhNIjoiaG1hY1NIQTI1NiIsIlRFTkFOVF9JRCI6ImZpbnBsdXMiLCJVU0VSX0tFWSI6IjhhODU4NmVlNzU2YTg2YzYwMTc1OWQ0Y2YxNzgzMDFlIn0'
       )
-    domain          =   values.DOMAIN
-    algorithm       =   values.ALGORITHM
-    tenant          =   values.TENANT_ID
-    user_key        =   values.USER_KEY
+    array = output.split(",")
+    count = 0; arr = []
+    until count == array.length
+      arr << array[count].split(":")
+      count +=1
+    end
+    arr.to_h
   end
 end
